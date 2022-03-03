c83e3750c41404984919d2181f5b8385299e6b66
Friday Okorie <bitdoctrine@gmail.com>
2022-03-03 08:10:26 +0100

    working on displaying entries on the dom

--- index.js
+++ index.js
@@ -1,40 +1,102 @@
-const expenditure = [];
+const dailyExpenditure = [
+  { 
+    expenseTime: "7:37:55 AM", 
+    name: "234565643", 
+    price: "34567876543" },
+  {
+    expenseTime: "7:41:01 AM",
+    name: "76543",
+    price: "3245675432",
+  },
+];
+
+const mainRecord = [];
 /*an expenditure is an object which represents instances of spending in key , value pairs. And each instance of spending contains other useful informations like date, time, amounts spent e.t.c */
 const form = document.getElementById("add-form");
 const addExpenseHandler = document.getElementById("submit");
-const closeModalHandler = document.getElementById('close-modal');
-const showModalHandler = document.getElementById('add');
-const modal = document.getElementById('modal');
+const closeModalHandler = document.getElementById("close-modal");
+const showModalHandler = document.getElementById("add");
+const modal = document.getElementById("modal");
 
 function showModal() {
-  modal.classList.remove('hidden')
+  modal.classList.remove("hidden");
 }
 function hideModal() {
-  modal.classList.add('hidden');
+  modal.classList.add("hidden");
 }
 
-showModalHandler.addEventListener('click', showModal);
-closeModalHandler.addEventListener('click', hideModal);
+if (showModalHandler) {
+  showModalHandler.addEventListener("click", showModal);
+}
 
+if (closeModalHandler) {
+  closeModalHandler.addEventListener("click", hideModal);
+}
 
-addExpenseHandler.addEventListener("click", function (e) {
-  e.preventDefault();
-  const expensePrice = document.getElementById("price").value;
-  const expenseName = document.getElementById("name").value;
-  if (expenseName.length <= 0 || expensePrice.length <= 0) {
-    alert("Name and Price are both required for this to work");
-    return
-  } else {
-    const expenseData = {
-      name: expenseName,
-      price: expensePrice,
+if (addExpenseHandler) {
+  addExpenseHandler.addEventListener("click", function (e) {
+    e.preventDefault();
+    const expensePrice = document.getElementById("price").value;
+    const expenseName = document.getElementById("name").value;
+    if (expenseName.length <= 0 || expensePrice.length <= 0) {
+      alert("Name and Price are both required for this to work");
+      return;
+    } else {
+      const expenseData = {
+        name: expenseName,
+        price: expensePrice,
+        expenseTime: `${new Date().toLocaleTimeString()}`,
+      };
+
+      dailyExpenditure.push(expenseData);
+      console.log(dailyExpenditure);
+      form.reset();
+      hideModal();
+    }
+  });
+}
+
+function MainRecordHandler() {
+  const date = new Date();
+
+  if (date.getHours() === 7 && date.getMinutes() <= 59) {
+    const record = dailyExpenditure;
+    const EntryDate = date.toLocaleDateString();
+
+    const entryObject = {
+      date: EntryDate,
+      entryRecord: record,
     };
-    expenditure.push(expenseData);
-    console.log(expenditure);
-    form.reset();
-    hideModal();
+
+    mainRecord.push(entryObject);
+    console.log(mainRecord);
+
   }
-});
+}
+
+MainRecordHandler();
 
 
+// setInterval( MainRecordHandler(), 60000);
 
+const entries = document.getElementById("entries-section");
+
+function entriesHandler() {
+  if (mainRecord.length === 0) {
+    entries.innerHTML = `<h1>
+  Still waiting for your first entry
+                         </h1>`;
+  } else {
+    mainRecord.forEach(item => {
+      const entry = document.createElement("div");
+      entry.className = "entry";
+      entry.innerHTML = `<h3><a href="#">${item.date}</a></h3>`;
+      entries.append(entry);
+    })
+  }
+}
+
+
+if (entries) {
+  window.addEventListener("load", entriesHandler);
+}
